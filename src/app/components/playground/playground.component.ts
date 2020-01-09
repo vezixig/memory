0d5e793d607127a54662c41ad8897e640b4615e7
Sebastian Born <sebastian.born@edeka.de>
2020-01-09 15:57:50 +0100

    Changed playfield generation

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -2,30 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { Tile } from 'src/app/components/playground/tile.class';
 
 @Component({
-  selector: 'app-playground',
-  templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.scss']
+    selector: 'app-playground',
+    templateUrl: './playground.component.html',
+    styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent implements OnInit {
+    public tiles: Tile[][] = [[]];
 
-  public tiles: Tile[][] = [[]];
-  
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-    this.generatePlayfield(5, 5);
-  }
+    ngOnInit() {
+        this.generatePlayfield(4, 4);
+    }
 
-  private generatePlayfield(width: number, height: number) {
-    const line: Tile[] = [];
-    line.push({image: 'cat_01.png'});
-    line.push({image: 'cat_01.png'});
-    line.push({image: 'cat_01.png'});
-    line.push({image: 'cat_01.png'});
+    /**
+     * Generate a new playfield
+     * @param width field width in columns
+     * @param height field height in rows
+     */
+    private generatePlayfield(width: number, height: number) {
+        const line: Tile[] = this.generateCards(width, height);
+        // split card-array in seperate rows
+        this.tiles = Array.from({ length: height }, (_, ih) =>
+            Array.from({ length: width }, (_, iw) => line[ih * width + iw])
+        );
+    }
 
-    this.tiles.push(line);
-    this.tiles.push(line);
-    this.tiles.push(line);
-    this.tiles.push(line);
-  }
+    /**
+     * Create cards, two per image
+     * @param width field width in columns
+     * @param height field height in rows
+     */
+    private generateCards(width: number, height: number): Tile[] {
+        const line = Array.from(
+            { length: Math.floor((width * height) / 2) },
+            (_, i) => ({
+                id: i,
+                image: 'cat_' + (i + 1).toString().padStart(2, '0') + '.png'
+            })
+        ).reduce((a, c) => [...a, c, c], []);
+        return this.shuffle(line);
+    }
+
+    /**
+     * Uses the Fiser-Yates Algorithm to shuffle an array
+     * @param arr Array to shuffle
+     */
+    private shuffle<T>(arr: Array<T>): Array<T> {
+        // Fisher-Yates Shuffle
+        for (let i = arr.length - 1; i > 0; i--) {
+            const target = Math.floor(Math.random() * i);
+            const tmp = arr[target];
+            arr[target] = arr[i];
+            arr[i] = tmp;
+        }
+        return arr;
+    }
 }
--- src/app/components/playground/tile.class.ts
+++ src/app/components/playground/tile.class.ts
@@ -1,3 +1,4 @@
 export interface Tile {
+    id: number;
     image: string;
 }
